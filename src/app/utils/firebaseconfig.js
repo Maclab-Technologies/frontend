bfbb94d05706d2de404238860944c6af3a75870b
somod-gif <eniolabadmus351@gmail.com>
2025-02-26 06:43:12 +0100

    products page 2

--- src/app/utils/firebaseconfig.js
+++ src/app/utils/firebaseconfig.js
@@ -1,25 +1,20 @@
 // firebaseconfig.js 
-
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZBVEdnLI83-Rbe1mquehYigoDgvXqZuU",
-  authDomain: "minutes-prints-local.firebaseapp.com",
-  projectId: "minutes-prints-local",
-  storageBucket: "minutes-prints-local.firebasestorage.app",
-  messagingSenderId: "349577346688",
-  appId: "1:349577346688:web:c6ff2a2b2b5d35015db065",
-  measurementId: "G-P7P40SMF6N"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Ensure Firebase only runs on the client
-let app;
-let auth;
-
-if (typeof window !== "undefined") {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-}
+// Initialize Firebase only if it's not already initialized
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const auth = getAuth(app);
 
 export { app, auth };
+
