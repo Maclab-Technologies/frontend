9c610084135918457c0be8e428f998c5145c01f7
somod-gif <eniolabadmus351@gmail.com>
2025-08-03 11:04:22 +0100

    admin restructured

--- src/app/Admin/components/shared/Header.js
+++ src/app/Admin/components/shared/Header.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAdmin } from '../../context/AdminContext';
-import { FaBars, FaUser } from 'react-icons/fa';
+import { FaBars, FaUser, FaSignOutAlt } from 'react-icons/fa';
 
 export default function Header() {
   const { 
